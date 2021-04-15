31177efec6514072b09ad7cdccb6c28d58d83e48
nicolestella <nicolestella@users.noreply.github.com>
2021-04-16 02:50:33 +1000

    done details section

--- pages/client/dashboard/makings/[makingID].js
+++ pages/client/dashboard/makings/[makingID].js
@@ -1,10 +1,32 @@
-// import { useRouter } from 'next/router'
+import React from 'react'
+import { useRouter } from 'next/router'
 
-// const Post = () => {
-// 	const router = useRouter()
-// 	const { makingID } = router.query
+//  import custom components
+import list from '@/assets/dummy/makings'
+import Index from './index'
+import MakingOverview from '@/components/clientProjects/MakingOverview'
 
-// 	return <p>Post: {makingID}</p>
-// }
+const Post = () => {
+	const router = useRouter()
+	const { makingID } = router.query
 
-// export default Post
+	const [foundMaking, setFoundMaking] = React.useState(false)
+	const making = list.makings.find((item) => item.id === makingID)
+
+	React.useEffect(() => {
+		// Check if item exists
+		if (making) {
+			setFoundMaking(true)
+		}
+	})
+
+	return (
+		<Index>
+			{foundMaking
+				? (<MakingOverview making={making}/>)
+				: (<p>Item not found</p>)}
+		</Index>
+	)
+}
+
+export default Post
--- pages/client/dashboard/makings/index.js
+++ pages/client/dashboard/makings/index.js
@@ -1,13 +1,62 @@
-import React from 'react'
-import Layout from '@/layouts/Dashboard'
-import Typography from '@material-ui/core/Typography'
-import Post from './[makingID]'
-import list from '@/assets/dummy/makings'
+// The page that shows the full details of the selected client project
 
-const Index = () => {
+/* eslint-disable react/prop-types */
+
+// import styling
+import theme from '@/config/theme'
+import { makeStyles } from '@material-ui/core/styles'
+
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
+import AppBar from '@material-ui/core/AppBar'
+
+// import custom components
+import Layout from '@/layouts/Dashboard'
+
+const useStyles = makeStyles({
+	appBar: {
+		display: 'flex',
+		flexDirection: 'row',
+		alignItems: 'center',
+		padding: theme.spacing(1),
+		backgroundColor: theme.palette.primary.light,
+		width: '100%',
+	},
+	notificationText: {
+		color: theme.palette.primary.contrastText,
+		flexGrow: 1,
+	},
+	button: {
+		position: 'relative',
+	},
+})
+const Index = (props) => {
+	const classes = useStyles()
+	const notification = true
+
 	return (
 		<Layout>
-			<Typography>Hello</Typography>
+			<Grid container>
+
+				{/* If there is a notification, show notification appbar */}
+				{notification
+					? (
+						<Grid item xs={12}>
+							<AppBar position="static" className={classes.appBar} elevation={0}>
+								<Typography className={classes.notificationText}>
+							A notification from Meydit
+								</Typography>
+								<Button className={classes.button}>ACTION</Button>
+							</AppBar>
+						</Grid>
+					)
+					: null}
+				<Grid item xs={12}>
+					{props.children}
+				</Grid>
+			</Grid>
 		</Layout>
 	)
 }
