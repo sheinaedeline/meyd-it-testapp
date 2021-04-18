8d3e670dc0c20bf6df7178d0e5a4338a07f2d7cc
nicolestella <nicolestella@users.noreply.github.com>
2021-04-18 21:13:23 +1000

    primary light is now white

--- config/theme.js
+++ config/theme.js
@@ -3,7 +3,7 @@ import { createMuiTheme } from '@material-ui/core/styles'
 const theme = createMuiTheme({
 	palette: {
 		primary: {
-			light: '#FDFCFE',
+			light: '#fff',
 			main: '#F0ECF8',
 			dark: '#DACFED',
 			contrastText: '#000',
