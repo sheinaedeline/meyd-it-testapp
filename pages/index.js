1ab48b726bbe0cf8310f1138dc4738576cc5d57c
nicolestella <nicolestella@users.noreply.github.com>
2021-09-28 15:44:03 +1000

    added info slide thing

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@
 import Layout from '@/layouts/Default'
 // import custom components
 import Gallery from '@/components/homepage/Gallery'
-import IconGrid from '@/components/homepage/IconGrid'
 import Hero from '@/components/homepage/Hero'
+import InfoSlide from '@/components/homepage/InfoSlide'
 import Values from '@/components/homepage/Values'
 // import content
 import GalleryItems from '@/content/GalleryItems'
@@ -14,7 +14,7 @@ export default function Home () {
 	return (
 		<Layout>
 			<Hero />
-			<IconGrid />
+			<InfoSlide />
 			<Values />
 			<Gallery galleryItems={GalleryItems} photoOnly={false} isCollection={true}/>
 		</Layout>
