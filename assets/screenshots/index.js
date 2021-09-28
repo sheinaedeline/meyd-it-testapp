d7d26f655cd7366d46a1cae374d89750cf1cede6
nicolestella <nicolestella@users.noreply.github.com>
2021-09-28 10:22:35 +1000

    taken screenshots

---
+++ assets/screenshots/index.js
@@ -0,0 +1,13 @@
+import CollectionsScreenshot from './collections.PNG'
+import InspirationsScreenshot from './inspirations.PNG'
+import MeasurementsScreenshot from './measurements.PNG'
+import TagsScreenshot from './tags.PNG'
+import TimelineScreenshot from './timeline.PNG'
+
+module.exports = {
+	CollectionsScreenshot,
+	InspirationsScreenshot,
+	MeasurementsScreenshot,
+	TagsScreenshot,
+	TimelineScreenshot,
+}
